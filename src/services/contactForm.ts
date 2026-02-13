18dd9d739c7b426a62aaf7cff7fb3babd694de16
Rulikovskij <castolo1313@gmail.com>
2026-02-13 17:42:18 +0000

    1

--- src/services/contactForm.ts
+++ src/services/contactForm.ts
@@ -12,7 +12,7 @@ export async function submitContactForm(data: ContactFormData): Promise<void> {
   const response = await fetch(webhookUrl, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "text/plain",
     },
     body: JSON.stringify({
       ...data,
